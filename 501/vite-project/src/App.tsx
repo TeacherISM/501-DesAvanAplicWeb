ffe7ccd6cae71aa034fecf2805a042a57f4e7bb3
HJZR2004 <A01027743@tec.mx>
2025-04-03 19:06:54 -0600

    Se agregó el botón de Log-in, ademas de la pagina log in, falta corregir las dependencias porque en la primera implementacion no se cumplieron con los requerimientos

--- 501/vite-project/src/App.tsx
+++ 501/vite-project/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+//botton
+import Button from './class2/Components/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate(); // Inicializa el hook useNavigate
+
+  const handleLoginClick = () => {
+    navigate('/login'); // Redirige a la ruta /login
+  };
 
   return (
     <>
@@ -18,9 +27,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div className="botton-container">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+
+          {/* Cambia el onClick para redirigir al login */}
+          <Button label="Log in" onClick={handleLoginClick} />
+        </div>
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +44,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
