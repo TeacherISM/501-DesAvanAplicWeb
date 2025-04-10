e21cdcf011622836c03f3a794f29b44a864b5081
Miguel Soria <a01028033@tec.mx>
2025-04-10 11:31:03 -0600

    feat: menu htmls work as intended and redirect to the empty tsx files. Implementing the already developed code for each section should be enough to finish, must add styles.css file for the html menu.

--- 501/vite-project/src/App.tsx
+++ 501/vite-project/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './class2/pages/Login'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./class2/pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [showLogin, setShowLogin] = useState(false)
+  const [count, setCount] = useState(0);
+  const [showLogin, setShowLogin] = useState(false);
 
   if (showLogin) {
-    return <Login />
+    return <Login />;
   }
 
   return (
@@ -32,18 +32,15 @@ function App() {
         </p>
       </div>
       <div className="card">
-        <button 
-          onClick={() => setShowLogin(true)}
-          style={{ backgroundColor: '#4CAF50', color: 'white', marginTop: '20px' }}
-        >
-          A01028033's Login Button!
-        </button>
+        <a href="/A01028033/menu/milestoneMenu.html">
+          <button>A01028033's Menu Button!</button>
+        </a>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
