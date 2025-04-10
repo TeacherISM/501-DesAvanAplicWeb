4ba7afcb70dff912ba141686c3408df8ea374004
A <A01027920@tec.mx>
2025-04-10 14:14:09 -0600

    added some style

--- 501/vite-project/src/App.tsx
+++ 501/vite-project/src/App.tsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <a href={'/A01027920/Home.html'} className='buttonlink'>A01027920 Menu</a>
     </>
   )
 }
---
+++ 501/vite-project/src/class2/A01027920/HomePage.tsx
@@ -0,0 +1,5 @@
+// enviar un pr con una carpeta con la matrícula para cada clase
+// cada carpeta tiene su implementación de los .tsx
+//el .tsx va a tener la implementación final de cada clase
+//el único archivo que se puede modificar es App.tsx
+//el menú en src/classX
