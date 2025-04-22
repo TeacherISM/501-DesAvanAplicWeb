6be0d85ad58d698f0142745d7cee52f6e2b079f1
A <A01027920@tec.mx>
2025-04-21 20:57:05 -0600

    remove folder

--- vite-project/src/class3/A01027920/NewLoginEntry.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Login from './NewLogin'
-
-createRoot(document.getElementById('login-root')!).render(
-  <StrictMode>
-    <Login />
-  </StrictMode>,
-)
