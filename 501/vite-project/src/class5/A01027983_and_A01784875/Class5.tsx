85c8fff9ee265aedbf9f62dac4d84b5d53e8f6e3
BassedWarrior <116653989+BassedWarrior@users.noreply.github.com>
2025-04-25 16:52:29 -0600

    chore: rename files to proper directory name.
    Same as last time, it was important that both IDs were present in the
    directory.

--- 501/vite-project/src/class5/A01027983_and_A01784875/Class5.tsx
+++ 501/vite-project/src/class5/A01027983_and_A01784875/Class5.tsx
@@ -8,11 +8,11 @@ function Class5() {
       <div>
         <h1>Menu</h1>
         <div>
-          <Button label="Class4" onClick={() => document.location = "/src/class4/A01027983/"} />
-          <Button label="Class5" />
-          <Button label="Class6" onClick={() => document.location = "/src/class6/A01027983/"} />
-          <Button label="Milestone1" onClick={() => document.location = "/src/class1/A01027983/"} />
           <Button label="Main Page" onClick={() => document.location= "/"} />
+          <Button label="Milestone1" onClick={() => document.location = "/src/class1/A01027983/"} />
+          <Button label="Class4" onClick={() => document.location = "/src/class4/A01027983_and_A01784875/"} />
+          <Button label="Class5" />
+          <Button label="Class6" onClick={() => document.location = "/src/class6/A01027983_and_A01784875/"} />
         </div>
       </div>
       <h1>Class 5 Example A01027983 and A01784875</h1>
