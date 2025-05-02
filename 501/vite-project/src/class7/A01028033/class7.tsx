fc42ebd7396da10fca03b4c8cf09695e9a707079
Gwenvito <toootiz.killer@gmail.com>
2025-05-02 15:39:26 -0600

    Menu clase7

---
+++ 501/vite-project/src/class7/A01028033/class7.tsx
@@ -0,0 +1,13 @@
+// Script with the activities of class 1
+import { useState } from "react";
+import "/src/A01028033/styles.css";
+
+
+
+const Class4 = () => {
+  return (
+    <p>hola papu</p>
+  );
+};
+
+export default Class4;
---
+++ 501/vite-project/src/class7/A01028033/class7Render.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Class7 from "./class7";
+import "/src/index.css";
+
+createRoot(document.getElementById("class7Div")!).render(
+  <StrictMode>
+    <Class7 />
+  </StrictMode>,
+);
