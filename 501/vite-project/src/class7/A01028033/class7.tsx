86b1246b8a13f1ebf4c16bcd4bbf90bf3d6c2f68
Gwenvito <toootiz.killer@gmail.com>
2025-05-02 16:28:25 -0600

    coneccion

--- 501/vite-project/src/class7/A01028033/class7.tsx
+++ 501/vite-project/src/class7/A01028033/class7.tsx
@@ -1,12 +1,23 @@
 // Script with the activities of class 1
 import { useState } from "react";
 import "/src/A01028033/styles.css";
+import RealTimeNotifications from "./RealTimeNotifications";
 
 
 
 const Class4 = () => {
   return (
-    <p>hola papu</p>
+    <div id="root">
+      <h1>WebSockets for Real-Time Updates</h1>
+
+      <section style={{ marginBottom: "40px" }}>
+        <RealTimeNotifications />
+      </section>
+
+      <a href="/public/A01028033/menu/milestoneMenu.html">
+        <button>Back to menu</button>
+      </a>
+    </div>
   );
 };
 
