6649bdf56cbdd190b39fed29bdeae972c4bf29c8
BassedWarrior <116653989+BassedWarrior@users.noreply.github.com>
2025-05-02 20:19:40 -0600

    build: utilize `vitest` instead of `jest`.
    `vitest` has considerably better compatibility with Vite + React + ESM +
    TSX. And configuring an running it is as minimal as it can get.

--- 501/vite-project/jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest', // Babel will still handle JS/JSX and TS/TSX
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'], // Include 'ts' and 'tsx'
-};
